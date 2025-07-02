0153f47ada484f986cd1c35322447ed737180f33
DroidChief <lovedayv7@gmail.com>
2025-07-02 16:02:03 +0100

    chore: define dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_cart/config.dart';
 
 void main() {
-  final config = AppConfig(
-    appName: 'Instance A',
-    localDbName: 'instance_a_db',
-  );
+  final config = AppConfig(appName: 'Instance A', localDbName: 'instance_a_db');
   runApp(MyApp(config: config));
 }
 
@@ -20,8 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text( 'Flutter Demo Home Page'),
+      home: const Text('Flutter Demo Home Page'),
     );
   }
 }
-
--- lib/main_b.dart
+++ lib/main_b.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_cart/config.dart';
 
 void main() {
-  runApp(const MyApp());
+  final config = AppConfig(appName: 'Instance B', localDbName: 'instance_b_db');
+  runApp(MyApp(config: config));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AppConfig config;
+  const MyApp({super.key, required this.config});
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text( 'Flutter Demo Home Page'),
+      home: const Text('Flutter Demo Home Page'),
     );
   }
 }
-
