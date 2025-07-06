5e9847b0494ccefb7a8da3df599c321b339e934e
DroidChief <lovedayv7@gmail.com>
2025-07-06 18:43:16 +0100

    fix: ensure the Isar core is initialized before the app runs

--- lib/main_a.dart
+++ lib/main_a.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_cart/app.dart';
 import 'package:go_cart/config.dart';
+import 'package:isar/isar.dart';
 
-void main() {
-  final config = AppConfig(appName: 'Instance A', localDbName: 'instance_a_db');
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Isar.initializeIsarCore(download: true);
+
+  final config = AppConfig(appName: 'Instance A', localDbName: 'instance_a_db');
   runApp(MyApp(config: config));
 }
--- lib/main_b.dart
+++ lib/main_b.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_cart/app.dart';
 import 'package:go_cart/config.dart';
+import 'package:isar/isar.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Isar.initializeIsarCore(download: true);
+  
   final config = AppConfig(appName: 'Instance B', localDbName: 'instance_b_db');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp(config: config));
